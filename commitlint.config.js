1ce62666344635a40a98c3088316f115518ab9e7
wondonghwi <ehdehd1289@naver.com>
2023-09-17 20:40:45 +0900

    feat: 첫글자 대문자도 가능하도록 commitlint 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'subject-case': [0], // subject-case 옵션 비활성화
+    },
+  };
+  
