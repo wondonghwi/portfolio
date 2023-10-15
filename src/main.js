2aed4ddd37884025220fbc73535311798a09e66f
wondonghwi <ehdehd1289@naver.com>
2023-10-15 12:15:56 +0900

    feat: projects js 작업

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { headerScrollEffect } from './headerScrollEffect.js';
 import { homeScrollOpacity } from './homeScrollOpacity.js';
 import { arrowScrollDisplay } from './arrowScrollDisplay.js';
 import { navbarToggle } from './navbarToggle.js';
+import { projects } from './projects.js';
 
 headerScrollEffect();
 homeScrollOpacity();
 arrowScrollDisplay();
 navbarToggle();
+projects();
--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,36 @@
 'use strict';
+
+export const projects = () => {
+  const categories = document.querySelector('.categories');
+  const projectList = document.querySelectorAll('.project');
+  const projectListContainer = document.querySelector('.projects');
+
+  categories.addEventListener('click', (e) => {
+    const filter = e.target.dataset.category;
+
+    if (!filter) return;
+
+    handleActiveSelection(e.target);
+    filterProjects(filter, projectList, projectListContainer);
+  });
+};
+
+const handleActiveSelection = (target) => {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+};
+
+const filterProjects = (filter, projectList, projectListContainer) => {
+  projectList.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectListContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectListContainer.classList.remove('anim-out');
+  }, 250);
+};
